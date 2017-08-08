3b7fd463db2a99acc8686d8c108f41aaa2753643
barancankatesen <barancankatesen@hotmail.com>
2017-08-08 16:30:29 +0300

    Giydirmeye Devam edildi Login Logout İşlemleri Tamamlandı Admin Layout İçin Script tabanlı yetki denetimi yazıldı Admin Controller İçin Yetki denetimi yazıldı
    Giydirmeye Devam edildi Login Logout İşlemleri Tamamlandı Admin Layout İçin Script tabanlı yetki denetimi yazıldı Admin Controller İçin Yetki denetimi yazıldı

--- E-Commerce/Controllers/HomeController.cs
+++ E-Commerce/Controllers/HomeController.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using E_Commerce.Models.DataModel;
+using E_Commerce.Models.Helper;
+using E_Commerce.CF;
 
 namespace E_Commerce.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
+
+        Context _db = Connection.Connect();
+
         public ActionResult Index()
         {
             
@@ -16,15 +22,42 @@ namespace E_Commerce.Controllers
         }
         public ActionResult Login()
         {
+            if ((string)Session["AdiSoyadi"] !=null&&(string)Session["AdiSoyadi"] !="")
+            {
+                return RedirectToAction("Index", "Home");
+            }
             return View();
         }
         [HttpPost]
         public ActionResult Login(FormCollection frm)
         {
-            string username = frm.Get("email");
-            string password = frm.Get("password");
+            try
+            {
+                string ViewUserMail = frm.Get("email");
+                string ViewUserPassword = frm.Get("password");
+                BaseUser ToLogin = _db.Users.FirstOrDefault(x => x.EMail == ViewUserMail && x.Password == ViewUserPassword);
+                if (ToLogin != null)
+                {
+                    Session.Add("AdiSoyadi", (ToLogin.Name + " " + ToLogin.Surname));
+                    Session.Add("UserID", ToLogin.BaseUserID);
+                    Session.Add("Authority", ToLogin.Authority);
+                }
+            }
+            catch (Exception e)
+            {
 
-            return View();
+                ViewBag.Mesaj = e.Message;
+                return View();
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult LogOut()
+        {
+            Session.Remove("AdiSoyadi");
+            Session.Remove("UserID");
+            Session.Remove("Authority");
+            return RedirectToAction("Index", "Home");
         }
     }
 }
