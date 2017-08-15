d935d05b385ae5b954de13e7f175860710f6cfc3
barancankatesen <barancankatesen@users.noreply.github.com>
2017-08-15 17:46:00 +0300

    Admin controller için yetki denetimi tekrar yazıldı Admin layoutta birkaç noktaya link verildi Admin adsoyad güncellemeye girildi

--- E-Commerce/Controllers/AdminController.cs
+++ E-Commerce/Controllers/AdminController.cs
@@ -3,28 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using E_Commerce.Models.DataModel;
+using E_Commerce.CF;
+using E_Commerce.Models.Helper;
 
 namespace E_Commerce.Controllers
 {
     public class AdminController : Controller
     {
-        private ActionResult GirisDenetle(ControllerContext controllerContext, ViewResult viewResult)
+        Context _db = Connection.Connect();
+        public bool AuthorityCheck()
         {
             if ((string)Session["Authority"] != "Admin" && (string)Session["Authority"] != "Master")
             {
-                return RedirectToAction("Index", "Home");
+                return true;
             }
-            return viewResult;
-
+            return false;
         }
         // GET: Admin
         public ActionResult Index()
         {
-            GirisDenetle(ControllerContext,View()).ExecuteResult(ControllerContext);
+            if (AuthorityCheck())
+            {
+                return RedirectToAction("Index", "Home");
+
+            }
             return View();
-            
+
         }
 
-      
+        public ActionResult Settings()
+        {
+            if (AuthorityCheck())
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        public ActionResult ChangeNameSurname()
+        {
+            if (AuthorityCheck())
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            UserNameSurname ToEdit = new UserNameSurname();
+            ToEdit.ID =Convert.ToInt32(Session["UserID"]);
+            
+
+            var ToEditFromDb  =_db.Users.Where(x => x.BaseUserID == Convert.ToInt32(Session["UserID"])).Select(x => new  {x.Name, x.Surname }).FirstOrDefault();
+            ToEdit.Name = ToEditFromDb.Name;
+            ToEdit.Surname = ToEditFromDb.Surname;
+          
+            return View(ToEdit);
+        }
+
+
     }
 }
